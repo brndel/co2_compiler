7d0837d3affc75aed7029483add9c7ac479a0fe8
brndel <ar.braendle@gmail.com>
2025-06-01 02:15:11 +0200

    fix: now use new builder to create condition in build_ir_ternary

--- src/ssa/construction/build_expr.rs
+++ src/ssa/construction/build_expr.rs
@@ -18,82 +18,24 @@ pub fn build_ir_expr<'a>(
         Expression::Num(num) => SsaValue::ImmediateNum(num.num()),
         Expression::Bool((value, _)) => SsaValue::ImmediateBool(value),
         Expression::Binary { a, op, b } => match op {
-            BinaryOperator::LogicAnd => {
-                build_ir_ternary(
-                    *a,
-                    *b,
-                    Expression::Bool((false, SimpleSpan::new(0, 0))),
-                    ctx,
-                    builder,
-                )
-                // let target = ctx.counter.next();
-
-                // let next_label = builder.next_label();
-                // let on_true = ctx.counter.next_block_label("and_true");
-                // let on_false = ctx.counter.next_block_label("and_false");
-
-                // let a = build_ir_expr(*a, ctx, builder);
-                // builder.end(
-                //     BasicBlockEnd::ConditionalJump {
-                //         condition: a,
-                //         on_true,
-                //         on_false,
-                //     },
-                //     ctx,
-                // );
-
-                // let mut false_scope = BlockBuilder::new(on_false, next_label, ctx);
-                // false_scope.push_instruction(SsaInstruction::Move {
-                //     target,
-                //     source: SsaValue::ImmediateNum(0),
-                // });
-                // false_scope.close(ctx);
-
-                // let mut true_scope = BlockBuilder::new(on_true, next_label, ctx);
-                // let b = build_ir_expr(*b, ctx, &mut true_scope);
-                // true_scope.push_instruction(SsaInstruction::Move { target, source: b });
-                // true_scope.close(ctx);
-
-                // SsaValue::Register(target)
-            }
-            BinaryOperator::LogicOr => {
-                    build_ir_ternary(
-                    *a,
-                    Expression::Bool((true, SimpleSpan::new(0, 0))),
-                    *b,
-                    ctx,
-                    builder,
-                )
-                // let target = ctx.counter.next();
-
-                // let next_label = builder.next_label();
-                // let on_true = ctx.counter.next_block_label("or_true");
-                // let on_false = ctx.counter.next_block_label("or_false");
-
-                // let a = build_ir_expr(*a, ctx, builder);
-                // builder.end(
-                //     BasicBlockEnd::ConditionalJump {
-                //         condition: a,
-                //         on_true,
-                //         on_false,
-                //     },
-                //     ctx,
-                // );
-
-                // let mut false_scope = BlockBuilder::new(on_false, next_label, ctx);
-                // let b = build_ir_expr(*b, ctx, &mut false_scope);
-                // false_scope.push_instruction(SsaInstruction::Move { target, source: b });
-                // false_scope.close(ctx);
-
-                // let mut true_scope = BlockBuilder::new(on_true, next_label, ctx);
-                // true_scope.push_instruction(SsaInstruction::Move {
-                //     target,
-                //     source: SsaValue::ImmediateNum(0),
-                // });
-                // true_scope.close(ctx);
-
-                // SsaValue::Register(target)
-            }
+            BinaryOperator::LogicAnd => build_ir_ternary(
+                *a,
+                *b,
+                Expression::Bool((false, SimpleSpan::new(0, 0))),
+                ctx,
+                builder,
+                "and_true",
+                "and_false",
+            ),
+            BinaryOperator::LogicOr => build_ir_ternary(
+                *a,
+                Expression::Bool((true, SimpleSpan::new(0, 0))),
+                *b,
+                ctx,
+                builder,
+                "or_true",
+                "or_false",
+            ),
             op => {
                 let a = build_ir_expr(*a, ctx, builder);
                 let b = build_ir_expr(*b, ctx, builder);
@@ -126,36 +68,15 @@ pub fn build_ir_expr<'a>(
 
             SsaValue::Register(target)
         }
-        Expression::Ternary { condition, a, b } => {
-            build_ir_ternary(*condition, *a, *b, ctx, builder)
-            // let target = ctx.counter.next();
-
-            // let next_label = builder.next_label();
-            // let on_true = ctx.counter.next_block_label("ternary_true");
-            // let on_false = ctx.counter.next_block_label("ternary_false");
-
-            // let condition = build_ir_expr(*condition, ctx, builder);
-            // builder.end(
-            //     BasicBlockEnd::ConditionalJump {
-            //         condition,
-            //         on_true,
-            //         on_false,
-            //     },
-            //     ctx,
-            // );
-
-            // let mut true_scope = BlockBuilder::new(on_true, next_label, ctx);
-            // let a = build_ir_expr(*a, ctx, &mut true_scope);
-            // true_scope.push_instruction(SsaInstruction::Move { target, source: a });
-            // true_scope.close(ctx);
-
-            // let mut false_scope = BlockBuilder::new(on_false, next_label, ctx);
-            // let b = build_ir_expr(*b, ctx, &mut false_scope);
-            // false_scope.push_instruction(SsaInstruction::Move { target, source: b });
-            // false_scope.close(ctx);
-
-            // SsaValue::Register(target)
-        }
+        Expression::Ternary { condition, a, b } => build_ir_ternary(
+            *condition,
+            *a,
+            *b,
+            ctx,
+            builder,
+            "ternary_true",
+            "ternary_false",
+        ),
     }
 }
 
@@ -165,15 +86,26 @@ fn build_ir_ternary<'a>(
     false_expr: Expression<'a>,
     ctx: &mut Context<'a>,
     builder: &mut BlockBuilder<'a>,
+    true_label: &'static str,
+    false_label: &'static str,
 ) -> SsaValue<'a> {
+    let next_label = builder.next_label();
+    let condition_label = ctx.counter.next_block_label("condition");
+    let on_true = ctx.counter.next_block_label(true_label);
+    let on_false = ctx.counter.next_block_label(false_label);
     let target = ctx.counter.next();
 
-    let next_label = builder.next_label();
-    let on_true = ctx.counter.next_block_label("ternary_true");
-    let on_false = ctx.counter.next_block_label("ternary_false");
-
-    let condition = build_ir_expr(condition, ctx, builder);
     builder.end(
+        BasicBlockEnd::Goto {
+            label: condition_label,
+        },
+        ctx,
+    );
+
+    let mut condition_builder = BlockBuilder::new(condition_label, next_label, ctx);
+
+    let condition = build_ir_expr(condition, ctx, &mut condition_builder);
+    condition_builder.close_with(
         BasicBlockEnd::ConditionalJump {
             condition,
             on_true,
@@ -184,12 +116,18 @@ fn build_ir_ternary<'a>(
 
     let mut true_scope = BlockBuilder::new(on_true, next_label, ctx);
     let true_value = build_ir_expr(true_expr, ctx, &mut true_scope);
-    true_scope.push_instruction(SsaInstruction::PhiMove { target, source: true_value });
+    true_scope.push_instruction(SsaInstruction::PhiMove {
+        target,
+        source: true_value,
+    });
     true_scope.close(ctx);
 
     let mut false_scope = BlockBuilder::new(on_false, next_label, ctx);
     let false_value = build_ir_expr(false_expr, ctx, &mut false_scope);
-    false_scope.push_instruction(SsaInstruction::PhiMove { target, source: false_value });
+    false_scope.push_instruction(SsaInstruction::PhiMove {
+        target,
+        source: false_value,
+    });
     false_scope.close(ctx);
 
     SsaValue::Register(target)
